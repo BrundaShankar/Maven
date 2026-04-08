ca820eeae227e1847b5fb23169b2d670fd2386eb
Brunda <131005645+BrundaShankar@users.noreply.github.com>
2026-04-08 15:30:06 +0530

    Add additional tests for Calculator addition method

--- src/test/java/com/mytest/app/CalculatorTest.java
+++ src/test/java/com/mytest/app/CalculatorTest.java
@@ -1,3 +1,4 @@
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,5 +8,20 @@ public class CalculatorTest {
     public void testAdd() {
         Calculator calc = new Calculator();
         assertEquals(5, calc.add(2, 3));
+         @Test
+    void testAddition() {
+        assertEquals(10, calculator.add(5,5));
+    }
+
+    @Test
+    void testAdditionDifferentValues() {
+        assertEquals(7, calculator.add(3,4));
+    }
+
+    @Test
+    void testAdditionNegative() {
+        assertEquals(-2, calculator.add(-1,-1));
+    }
+
     }
 }
